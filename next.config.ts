859ac8349a350c33d2b0f45f2d89e57a3571adc6
JS <csdnum0@gmail.com>
2026-09-22 23:34:02 +0900

    docs: next.config.ts에 한글 주석 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+// Next.js 프로젝트의 설정을 정의하는 객체
 const nextConfig: NextConfig = {
-  /* config options here */
+  // React Compiler를 활성화하면 React 코드를 자동으로 최적화할 수 있음
   reactCompiler: true,
 };
 
+// 위에서 만든 Next.js 설정을 외부에 내보냄
 export default nextConfig;
